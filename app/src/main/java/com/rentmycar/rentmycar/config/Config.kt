42b5ed73ddca77ff1c4aabdaf6f4bb0a5a744fba
Joris Jansen <j.jansen@ispnext.com>
2021-12-11 07:17:08 +0100

    Small changes

--- app/src/main/java/com/rentmycar/rentmycar/config/Config.kt
+++ app/src/main/java/com/rentmycar/rentmycar/config/Config.kt
@@ -3,5 +3,5 @@ package com.rentmycar.rentmycar.config
 object Config {
     const val BASE_URL = "http://10.0.2.2:8080/api/v1.0/"
     const val PREFERENCES = "preferences"
-    const val ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huLmRvZUBnbWFpbC5jb20iLCJyb2xlcyI6WyJVU0VSIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hcGkvdjEuMC9hdXRoL2xvZ2luIiwiZXhwIjoxNjM5Mjg1MDg4fQ.cRFBBDTyD2ngA5JpMg34IrSmkdmIrBlHko2czkCKDdA"
+    const val ACCESS_TOKEN = "accessToken"
 }
