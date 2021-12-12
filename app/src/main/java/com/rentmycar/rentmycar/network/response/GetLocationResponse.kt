f9d8a3aaad8c2c96b36be7506769cce9bd05f4e4
zafuso <71095771+zafuso@users.noreply.github.com>
2021-12-12 09:52:27 +0100

    locationlist now works

--- app/src/main/java/com/rentmycar/rentmycar/network/response/GetLocationResponse.kt
+++ app/src/main/java/com/rentmycar/rentmycar/network/response/GetLocationResponse.kt
@@ -2,14 +2,14 @@ package com.rentmycar.rentmycar.network.response
 
 data class GetLocationResponse(
     val id: Int,
-    val street: String,
-    val houseNumber: String,
-    val postalCode: String,
-    val city: String,
-    val country: String,
+    val street: String?,
+    val houseNumber: String?,
+    val postalCode: String?,
+    val city: String?,
+    val country: String?,
     val latitude: Float,
     val longitude: Float,
     val userId: Long,
     val createdAt: String,
-    val updatedAt: String
+    val updatedAt: String?
 )
