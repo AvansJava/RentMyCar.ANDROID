02a5f35422f6eb3566f77cd9e8f331fe61f8d371
zafuso <71095771+zafuso@users.noreply.github.com>
2021-12-13 08:37:56 +0100

    car create layout complete

--- app/src/main/java/com/rentmycar/rentmycar/fragment/CarCreateFragment.kt
+++ app/src/main/java/com/rentmycar/rentmycar/fragment/CarCreateFragment.kt
@@ -1,13 +1,24 @@
 package com.rentmycar.rentmycar.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.rentmycar.rentmycar.R
+import kotlinx.android.synthetic.main.fragment_car_create.*
 
 class CarCreateFragment: Fragment() {
+
+    override fun onResume() {
+        super.onResume()
+
+        val carTypes = resources.getStringArray(R.array.car_types)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.model_car_type_list_item, carTypes)
+        autoCompleteTextView.setAdapter(arrayAdapter)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
